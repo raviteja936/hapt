08cf654c8efc7ffc6e99aa20f1539c7c0a27b1f5
raviteja936 <uppalapati.teja@gmail.com>
2020-10-21 01:24:35 -0400

    added numpy apply function

--- src/features/feature_functions.py
+++ src/features/feature_functions.py
@@ -15,8 +15,7 @@ class Features:
         self.functions = [np.mean, np.std, np.amax, np.amin, np.quantile, feature_calculators.abs_energy, \
                           feature_calculators.absolute_sum_of_changes, feature_calculators.autocorrelation, \
                           feature_calculators.kurtosis, feature_calculators.skewness, \
-                          feature_calculators.longest_strike_above_mean, feature_calculators.longest_strike_below_mean]
-                          # feature_calculators.sample_entropy]
+                          feature_calculators.longest_strike_above_mean, feature_calculators.longest_strike_below_mean, feature_calculators.sample_entropy]
 
         self.function_args = {np.quantile: [[0.25, 0.75]], feature_calculators.autocorrelation: [5]}
         self.function_kwargs = {}
@@ -43,7 +42,7 @@ class Features:
                 args = self.function_args[f]
             if f in self.function_kwargs:
                 kwargs = self.function_kwargs[f]
-            curr_feat = np.reshape(np.apply_along_axis(f, 2, segments, *args, **kwargs), (segments.shape[0], -1))
+            curr_feat = np.nan_to_num(np.reshape(np.apply_along_axis(f, 2, segments, *args, **kwargs), (segments.shape[0], -1)))
             # print("F", i, curr_feat[:2])
             features = np.concatenate((features, curr_feat), axis=1)
         return features
