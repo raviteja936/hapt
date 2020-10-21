f23868fc0e04b7d96f7723a1637f2cb891d97464
raviteja936 <uppalapati.teja@gmail.com>
2020-10-21 19:33:08 -0400

    added numpy apply function

--- src/features/feature_functions.py
+++ src/features/feature_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
-# from src.features.rough import get_features, get_fft_values, get_psd_values, get_autocorr_values
+from src.features.rough import get_fft_values, get_psd_values, get_autocorr_values
 from tsfresh.feature_extraction import feature_calculators
+
 
 N = 50
 f_s = 50
@@ -12,26 +13,15 @@ denominator = 10
 
 class Features:
     def __init__(self):
-        self.functions = [np.mean, np.std, np.amax, np.amin, np.quantile, feature_calculators.abs_energy, \
-                          feature_calculators.absolute_sum_of_changes, feature_calculators.autocorrelation, \
-                          feature_calculators.kurtosis, feature_calculators.skewness, \
-                          feature_calculators.longest_strike_above_mean, feature_calculators.longest_strike_below_mean, feature_calculators.sample_entropy]
+        self.functions = [np.mean, np.std, np.amax, np.amin, np.quantile, feature_calculators.abs_energy,
+                          feature_calculators.absolute_sum_of_changes, feature_calculators.autocorrelation,
+                          feature_calculators.kurtosis, feature_calculators.skewness,
+                          feature_calculators.longest_strike_above_mean,
+                          feature_calculators.longest_strike_below_mean, feature_calculators.sample_entropy]
 
         self.function_args = {np.quantile: [[0.25, 0.75]], feature_calculators.autocorrelation: [5]}
         self.function_kwargs = {}
-
-    def get_signal_features(self, signal):
-
-        features = []
-
-            # signal_min = np.nanpercentile(signal[i, :], percentile)
-            # signal_max = np.nanpercentile(signal[i, :], 100 - percentile)
-            # mph = signal_min + (signal_max - signal_min) / denominator
-            # # print (signal[i, :].shape)
-            # features += get_features(*get_fft_values(signal[i, :]), mph)
-            # features += get_features(*get_psd_values(signal[i, :]), mph)
-            # features += get_features(*get_autocorr_values(signal[i, :]), mph)
-        return features
+        self.freq_functions = [get_fft_values, get_psd_values, get_autocorr_values]
 
     def get_features(self, segments):
         features = np.empty((segments.shape[0], 0), float)
@@ -42,7 +32,12 @@ class Features:
                 args = self.function_args[f]
             if f in self.function_kwargs:
                 kwargs = self.function_kwargs[f]
-            curr_feat = np.nan_to_num(np.reshape(np.apply_along_axis(f, 2, segments, *args, **kwargs), (segments.shape[0], -1)))
-            # print("F", i, curr_feat[:2])
+            curr_feat = np.reshape(np.apply_along_axis(f, 2, segments, *args, **kwargs), (segments.shape[0], -1))
+            curr_feat[curr_feat == np.inf] = 0
             features = np.concatenate((features, curr_feat), axis=1)
+
+        for j, f in enumerate(self.freq_functions):
+            curr_feat = np.reshape(np.apply_along_axis(f, 2, segments), (segments.shape[0], -1))
+            features = np.concatenate((features, curr_feat), axis=1)
+            # curr_feat = (curr_feat - curr_feat.mean(axis=0)) / (0.001 + curr_feat.std(axis=0))
         return features
--- src/features/rough.py
+++ src/features/rough.py
@@ -3,6 +3,8 @@ import numpy as np
 from detecta import detect_peaks
 from scipy.signal import welch
 
+percentile = 5
+denominator = 10
 
 def get_first_n_peaks(x, y, no_peaks=5):
     x_, y_ = list(x), list(y)
@@ -23,12 +25,18 @@ def get_fft_values(y_values, T=0.02, N=50):
     f_values = np.linspace(0.0, 1.0 / (2.0 * T), N // 2)
     fft_values_ = fft(y_values)
     fft_values = 2.0 / N * np.abs(fft_values_[0:N // 2])
-    return f_values, fft_values
+    signal_min = np.nanpercentile(y_values, percentile)
+    signal_max = np.nanpercentile(y_values, 100 - percentile)
+    mph = signal_min + (signal_max - signal_min) / denominator
+    return get_features(f_values, fft_values, mph)
 
 
 def get_psd_values(y_values, f_s=50):
     f_values, psd_values = welch(y_values, fs=f_s)
-    return f_values, psd_values
+    signal_min = np.nanpercentile(y_values, percentile)
+    signal_max = np.nanpercentile(y_values, 100 - percentile)
+    mph = signal_min + (signal_max - signal_min) / denominator
+    return get_features(f_values, psd_values, mph)
 
 
 def autocorr(x):
@@ -39,4 +47,7 @@ def autocorr(x):
 def get_autocorr_values(y_values, T=0.02, N=50):
     autocorr_values = autocorr(y_values)
     x_values = np.array([T * jj for jj in range(0, N)])
-    return x_values, autocorr_values
+    signal_min = np.nanpercentile(y_values, percentile)
+    signal_max = np.nanpercentile(y_values, 100 - percentile)
+    mph = signal_min + (signal_max - signal_min) / denominator
+    return get_features(x_values, autocorr_values, mph)
